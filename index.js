cfa1131e7f3b3a7b82394e6c513faad1df570672
Семаков Андрей <asemakov@htc-cs.ru>
2020-01-16 14:36:58 +0400

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const packageJSON = require('./package.json');
 const { config: { directories: { production: dir } } } = packageJSON;
 
 module.exports = {
-  detection: require(`${dir}/sa-detection`),
-  Closest: require(`${dir}/sa-slosest`),
-  GridAutoPlacement: require(`${dir}/sa-grid-autoplacement`)
+  detection: require(`./${dir}/sa-detection`),
+  Closest: require(`./${dir}/sa-slosest`),
+  GridAutoPlacement: require(`./${dir}/sa-grid-autoplacement`)
 };
